ab61f8de89d505dc9fb3cc63808868dc655f6b7b
dani2885 <dani2885@easv365.dk>
2017-11-27 10:51:00 +0100

    JavaDoc Song.be

--- MP3-Project/src/mp3/be/Song.java
+++ MP3-Project/src/mp3/be/Song.java
@@ -26,37 +26,68 @@ public class Song implements Serializable {
         this.category = category;
     
     }
-
+    
+    /**
+     * Get the title
+     * @param title 
+     */
     public String getTitle()
     {
         return title;
     }
-
+    
+    /**
+     * Set the title
+     * @param title 
+     */
     public void setTitle(String title)
     {
         this.title = title;
     }
 
+    /**
+     * Get the artist
+     * @param artist 
+     */
     public String getArtist()
     {
         return artist;
     }
 
+    
+    /**
+     * Set the artist
+     * @param artist 
+     */
     public void setArtist(String artist)
     {
         this.artist = artist;
     }
 
+    
+    /**
+     * Get the category
+     * @param category 
+     */
     public String getCategory()
     {
         return category;
     }
 
+    /**
+     * Set the category
+     * @param category 
+     */
     public void setCategory(String category)
     {
         this.category = category;
     }
     
+    
+    /**
+     * The "toString" method helps you to make the obj to text.
+     * @return 
+     */
     @Override
     public String toString()
     {
