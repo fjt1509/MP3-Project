749999aeecefac7cf3b53e1838336171fb0f296c
fjt1509 <fjt1509@hotmail.com>
2017-12-15 12:00:43 +0100

    play pause

--- MP3-Project/src/mp3/gui/controller/MainWindowController.java
+++ MP3-Project/src/mp3/gui/controller/MainWindowController.java
@@ -541,6 +541,7 @@ public class MainWindowController implements Initializable {
                 else 
                 {
                     mediaplayer.stop();
+                    mediaplayer.play();
                 }
             }
 
